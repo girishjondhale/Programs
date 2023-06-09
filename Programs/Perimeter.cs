e0a83d1d0d1b15fd3d090918c1ad4804326dde9d
Administrator <Administrator@DESKTOP-GARLJAI>
2023-06-09 11:37:38 -0700

    Add project files.

---
+++ Programs/Perimeter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Programs
+{
+    internal class Perimeter
+    {
+       public static void Main()
+        {
+            Console.WriteLine("enter length");
+           Decimal length= Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("enter Breadth");
+           Decimal Breadth= Convert.ToDecimal(Console.ReadLine());
+
+            decimal perimeter = 2 * (length + Breadth);
+            Console.WriteLine("perimeter of rectangle is" + perimeter);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Programs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter radius");
+            double rad=Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * rad * rad;
+            Console.WriteLine("Area of circle is:" + area);
+        }
+    }
+}
